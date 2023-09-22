2f6148384e01195fbdfc27b41465ab9a924731f3
younes amanay <younesamany@gmail.com>
2023-09-21 17:20:49 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,23 +12,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int k;
-	int n;
-
-	k  = 0;
-	while (dest[k] != '\0')
-	{
-		k++;
-	
-	}
-	n=0;
-	
-	while (src[n] != '\0'
-	{
-		dest[k] = src[n];
-		k++;
-		n++;
-	}
-	dest[k] = '\0';
+	int c,c2;
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2=0;src[c2];c2++)
+		dest[c++] = src[c2];
 	return (dest);
 }
